97f7666306899a065247e5bbddbaec613f33a55b
Burakcan Sezgin <burakcan.sezgin@gmail.com>
2021-02-02 18:23:22 +0300

    adapter modify

--- app/src/main/java/com/mtek/goarenopoc/ui/adapter/homefeed/HomeFeedAdapter.kt
+++ app/src/main/java/com/mtek/goarenopoc/ui/adapter/homefeed/HomeFeedAdapter.kt
@@ -1,23 +1,23 @@
 package com.mtek.goarenopoc.ui.adapter.homefeed
 
 
+import android.app.Activity
+import android.util.DisplayMetrics
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.mtek.goarenopoc.data.model.Data
 import com.mtek.goarenopoc.data.model.FeedModel
-import com.mtek.goarenopoc.data.network.response.FeedResponseModel
 
 class HomeFeedAdapter(
     private var items: ArrayList<FeedModel>,
-    private val onItemClickListener: ( newsModel: FeedModel)->Unit
+    private val onItemClickListener: (newsModel: FeedModel) -> Unit
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when(viewType){
 
-            LayoutType.Thumnail.id->{
-                    HolderThumbnail(parent)
+            LayoutType.Thumnail.id -> {
+                HolderThumbnail(parent)
             }
             else ->{
                     HolderOnlyText(parent)
@@ -26,10 +26,11 @@ class HomeFeedAdapter(
         }
     }
 
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder.itemViewType) {
             LayoutType.Thumnail.id -> {
-                (holder as HolderThumbnail).bind(items[position],onItemClickListener)
+                (holder as HolderThumbnail).bind(items[position], onItemClickListener)
             }
 
             else -> {
@@ -48,12 +49,16 @@ class HomeFeedAdapter(
     }
 
     override fun getItemViewType(position: Int): Int {
-        return if (items[position].postType == "TEXT"){
-            1
-        }else if(items[position].postType == "VIDEO"){
-            0
-        }else{
-            0
+        return when (items[position].postType) {
+            "TEXT" -> {
+                1
+            }
+            "VIDEO" -> {
+                0
+            }
+            else -> {
+                0
+            }
         }
     }
 
