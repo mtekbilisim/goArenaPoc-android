1dbe8d90d5ea6d71f177ecae4b038159507308ef
Özgür <sezginzgurr@gmail.com>
2021-02-03 12:18:50 +0300

    data Listing completed

--- app/src/main/java/com/mtek/goarenopoc/ui/fragment/dashboard/DashBoardFragment.kt
+++ app/src/main/java/com/mtek/goarenopoc/ui/fragment/dashboard/DashBoardFragment.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.LegendEntry
@@ -14,11 +17,15 @@ import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.formatter.LargeValueFormatter
+import com.mtek.goarenopoc.R
+import com.mtek.goarenopoc.base.BaseAdapter
 import com.mtek.goarenopoc.base.BaseFragment
+import com.mtek.goarenopoc.data.model.DashboardDataModel
 import com.mtek.goarenopoc.data.network.response.DashboardResponseModel
 import com.mtek.goarenopoc.data.network.response.ExpectionResponseModel
 import com.mtek.goarenopoc.databinding.FragmentDashBoardBinding
 import com.mtek.goarenopoc.utils.flag_error
+import com.mtek.goarenopoc.utils.manager.UserManager
 
 class DashBoardFragment :
     BaseFragment<FragmentDashBoardBinding, DashboardViewModel>(DashboardViewModel::class) {
@@ -31,13 +38,16 @@ class DashBoardFragment :
     private val yQualityGroup2: ArrayList<BarEntry> = arrayListOf()
     private val xAxisQualityValues: ArrayList<String> = arrayListOf()
 
+    private var adapter: BaseAdapter<DashboardDataModel>? = null
+    private var personelDataList: ArrayList<DashboardDataModel> = arrayListOf()
+
     private val observerMontlySales: Observer<DashboardResponseModel> = Observer {
         if (it != null) {
             yValueGroup2.clear()
-            val dataSales = it.data?.groupBy { productGroup ->
+            val dataSales = it.data!!.groupBy { productGroup ->
                 productGroup.product_group
             }
-            for (i in dataSales!!) {
+            for (i in dataSales) {
                 xAxisValues.add(i.key)
                 var amounts: Int = 0
                 for (j in i.value) {
@@ -47,14 +57,16 @@ class DashBoardFragment :
                 yValueGroup2.add(BarEntry(1f, floatArrayOf(1.toFloat(), amounts.toFloat())))
             }
             populateGraphData()
+            personelDataList = it.data as ArrayList<DashboardDataModel>
+            setAdapterData()
         }
+
     }
 
     private val observerTarget: Observer<ExpectionResponseModel> = Observer {
-        if (it != null) {
-            //if (it.data!![0].user?.employee_type == "MANAGER") viewModel.getMontlyTargetById(7) else viewModel.getMontlyTarget()
+        if (!it.data.isNullOrEmpty()) {
             yValueGroup1.clear()
-            val dataTarget = it.data!!.groupBy { expection ->
+            val dataTarget = it.data.groupBy { expection ->
                 expection.product_group
             }
             for (i in dataTarget) {
@@ -82,37 +94,63 @@ class DashBoardFragment :
     }
 
     private val observerShopQuality: Observer<DashboardResponseModel> = Observer {
-        if (it != null) {
-            it.data
-            val dataQuality = it.data?.groupBy { shopPerson ->
+        if (!it.data.isNullOrEmpty()) {
+
+            val dataQuality = it.data.groupBy { shopPerson ->
                 shopPerson.user?.first_name
             }
-            for (i in dataQuality!!) {
-                i.key?.let { it1 -> xAxisQualityValues.add(it1) }
-                var personQuality = 0
-                for (j in i.value) {
-                    personQuality += j.quantity
-                }
+            for (i in dataQuality) {
+                    i.key?.let { it1 -> xAxisQualityValues.add(it1) }
+                    var personQuality = 0
+                    for (j in i.value) {
+                        personQuality += j.quantity
+                    }
 
-                yQualityGroup2.add(BarEntry(1f, floatArrayOf(2.toFloat(), personQuality.toFloat())))
-            }
+                    yQualityGroup2.add(BarEntry(1f, floatArrayOf(2.toFloat(), personQuality.toFloat())))
+                }
             PersonelGraphData()
         }
-    }
 
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.getFilterByShopPersonel(3)
         viewModel {
-            getFilterByShopPersonel(3)
             monthlySales.observe(viewLifecycleOwner, observerMontlySales)
             responseTarget.observe(viewLifecycleOwner, observerTarget)
             responseShopQuailty.observe(viewLifecycleOwner, observerShopQuality)
-            getMontlySales()
-            getMontlyTarget()
+            getFilterByShopPersonel(3)
         }
+    }
 
+    override fun onResume() {
+        super.onResume()
+        viewModel {
+
+            getMontlySales()
+            if (UserManager.instance.user?.employee_type == "MANAGER") getMontlyTargetById(3) else getMontlyTarget()
+
+        }
+    }
+
+    private fun setAdapterData() {
+        adapter =
+            BaseAdapter(
+                requireContext(),
+                R.layout.row_item_dashbroard_data_grid,
+                personelDataList
+            ) { v, item, position ->
+                val txtProductGroup = v!!.findViewById(R.id.txtProductGroup) as TextView
+                val txtName = v.findViewById(R.id.txtTitle) as TextView
+                val txtProduct = v.findViewById(R.id.txtProduct) as TextView
+                val txtSell = v.findViewById(R.id.txtSell) as TextView
+
+                txtName.text = item.user?.first_name
+                txtProductGroup.text = item.product_group
+                txtProduct.text = item.product
+                txtSell.text = item.quantity.toString()
+            }
+        binding.recyclerView.adapter = adapter
     }
 
     private fun populateGraphData() {
@@ -235,6 +273,7 @@ class DashBoardFragment :
         legend.setCustom(legenedEntries)
 
         legend.yOffset = 2f
+        legend.textColor = Color.WHITE
         legend.xOffset = 2f
         legend.yEntrySpace = 0f
         legend.textSize = 10f
@@ -258,15 +297,16 @@ class DashBoardFragment :
         //Soldaki alan
         val leftAxis = binding.barChartStaff.axisLeft
         leftAxis.valueFormatter = LargeValueFormatter()
-        leftAxis.setDrawGridLines(false) // ekrani kare / dikdortgen seklinde ayirior grid olarak hersey daha net
+        leftAxis.setDrawGridLines(false)
         leftAxis.spaceTop = 1f
+        leftAxis.textColor = Color.WHITE
         leftAxis.axisMinimum = 0f
 
         binding.barChartStaff.data = barData
 
         binding.barChartStaff.barData.barWidth = 0.4f
         binding.barChartStaff.xAxis.axisMaximum =
-            0f + binding.barChartStaff.barData.getGroupWidth(0.1f, 0.05f) * 3 //kac grup yazin varsa
+            0f + binding.barChartStaff.barData.getGroupWidth(0.1f, 0.05f) * 3
         binding.barChartStaff.xAxis.axisMinimum = 0f
         binding.barChartStaff.description.text = ""
         binding.barChartStaff.xAxis.axisMaximum = 3f
@@ -276,16 +316,6 @@ class DashBoardFragment :
 
         binding.barChartStaff.invalidate()
 
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        flag_error("onAttach Dashboard")
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        flag_error("onDetach Dashboard")
     }
 
     override fun getViewBinding() = FragmentDashBoardBinding.inflate(layoutInflater)
